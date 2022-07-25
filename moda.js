1be05ba702980dd5a67337515486cb1b82ce422d
Emmanuel Bravo <e.b.f413@gmail.com>
2022-07-24 23:50:29 -0600

    Taller # calcular moda con una funcion y que trababaje con html

--- moda.js
+++ moda.js
@@ -1,33 +1,32 @@
-const lista1 = [
-    1, 
-    2,
-    3,
-    1,
-    2,
-    3,
-    4,
-    2,
-    2,
-    2,
-    1,
-];
+function calcularModa() {
+    // Se traen los datos del Html y los guardamos los valores en una variable
+    const entradaModa = document.getElementById("dataModa");
+    const modaValue = entradaModa.value;
 
-const lista1Count = {};
+    // convertimos los valores en un array de numeros puesto que son de tipo string 
+    let arrayModa = Array.from(modaValue.split(","), Number);
 
-lista1.map(
-    function (elemento) {
-        if (lista1Count[elemento]) {
-            lista1Count[elemento] += 1;
-        } else {
-            lista1Count[elemento] = 1;
+    // inicializamos el objeto en el cual organizaremos los elementos del array y en donde contaremos cuantas veces se repite
+    const listaCount = {};
+
+    arrayModa.map(
+        function (elemento) {
+            if (listaCount[elemento]) {
+                listaCount[elemento] += 1;
+            } else {
+                listaCount[elemento] = 1;
+            }
         }
-    }
-);
+    );
 
-const lista1Array = Object.entries(lista1Count).sort(
-    function (elementoA, elementoB) {
-        return elementoA[1] - elementoB[1]; 
-    }
-);
+    const listaArray = Object.entries(listaCount).sort(
+        function (elementoA, elementoB) {
+            return elementoA[1] - elementoB[1]; 
+        }
+    );
 
-const moda = lista1Array[lista1Array.length -1];
+    const moda = listaArray[listaArray.length -1];
+
+    const mostrarResultado = document.getElementById("resultadoModa");
+    mostrarResultado.innerText = `La moda es: ${moda[0]} que se repite un total de: ${moda[1]} veces`;
+}
